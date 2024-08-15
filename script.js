9af5c9ea4a9e67f54b9b25b25d47c0a1877997fb
lizfrohwein <165309084+lizfrohwein@users.noreply.github.com>
2024-08-15 08:31:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,14 @@
 /////////////////////////////////////////////////
 // Data
 const account1 = {
-  owner: 'Liz Frohwein',
+  owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: 'Patrick Flanagan',
+  owner: 'Jessica Davis',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
